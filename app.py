8621f538f4571b2b185c650d420df0bf62b9c016
dima <dprofe@yandex.ru>
2022-12-20 20:45:53 +0300

    add prefix

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import os
 import sys
 
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, url_for
 from werkzeug.utils import secure_filename
+
+is_server = os.path.exists('/SERVER/is_server')
 
 app = Flask(__name__)
 app.secret_key = "daghfdgmkfhjdfgsnasfasfa"
-if len(sys.argv) < 2 or sys.argv[1] == '':
-    app.upload_folder = './uploaded/'
-else:
+
+if len(sys.argv) >= 2 and sys.argv[1] != '':
     app.upload_folder = sys.argv[1]
+elif is_server:
+    app.upload_folder = '/SERVER/share'
+else:
+    app.upload_folder = './uploaded/'
+
+if is_server:
+    prefix = '/file-uploader'
+else:
+    prefix = ''
 
 
-@app.route('/')
-def index():
+@app.route(prefix + '/')
+def page_index():
     return render_template('index.html')
 
 
-@app.route('/loaded', methods=['POST'])
-def got():
+@app.route(prefix + '/loaded', methods=['POST'])
+def page_loaded():
     file = request.files['file']
 
     if not file:
@@ -27,8 +37,11 @@ def got():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.upload_folder, filename))
         flash('Success', 'color: green;')
-    return redirect('/')
+    return redirect(url_for('page_index'))
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8004)
+    if is_server:
+        app.run('127.0.0.1', port=8004)
+    else:
+        app.run('0.0.0.0', port=5000)
